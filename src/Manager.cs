4831362c7e98d9e7e0b5702bbb42de5c0b08ba21
The Black Wizard <chuckydu24@gmail.com>
2025-04-04 08:06:33 +0200

    chore(Manager): remove old code

--- src/Manager.cs
+++ src/Manager.cs
@@ -3,54 +3,8 @@
 namespace toastifyRise {
     internal class Manager {
         static void Main(string[] args) {
-            //Thread thread = new(new ThreadStart(Toaster));
-            //thread.Start();
-
-            //while (true) {
-            //    Thread.Sleep(60000);
-
-            //    GC.Collect();
-            //    GC.WaitForPendingFinalizers();
-            //}
-
             Toaster();
         }
-
-        //static void Toaster() {
-        //    Listener listener = new Listener();
-        //    Dictionary<string, string>? results = new Dictionary<string, string>();
-
-        //    while (true) {
-        //        string message = "", type = "info";
-        //        string? title = null;
-
-        //        Console.WriteLine("Listening...");
-        //        results = listener.Listen();
-
-        //        Console.WriteLine("Data received:");
-        //        for (int i = 0; i < results.Count; i++) {
-        //            Console.WriteLine(results.Keys.ElementAt(i) + ": " + results.Values.ElementAt(i));
-        //        }
-
-        //        if (results.ContainsKey("message") && !String.IsNullOrEmpty(results["message"]))
-        //            message = results["message"];
-
-        //        if (results.ContainsKey("title") && !String.IsNullOrEmpty(results["title"]))
-        //            title = results["title"];
-
-        //        if (results.ContainsKey("type") && !String.IsNullOrEmpty(results["type"]))
-        //            type = results["type"];
-
-        //        Toast.Toasting(message, title, type);
-
-        //        Console.WriteLine("");
-
-        //        results = null;
-
-        //        GC.Collect();
-        //        GC.WaitForPendingFinalizers();
-        //    }
-        //}
 
         public static void Toaster() {
             Listener listener = new Listener();
@@ -60,11 +14,6 @@ namespace toastifyRise {
                 string message = "", type = "info";
                 string? title = null;
                 bool persist = false;
-
-                // Console.WriteLine("Data received:");
-                // foreach (var kvp in results) {
-                //     Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-                // }
 
                 if (results.ContainsKey("message") && !String.IsNullOrEmpty(results["message"]))
                     message = results["message"];
@@ -79,8 +28,6 @@ namespace toastifyRise {
                     persist = bool.Parse(results["persist"]);
 
                 Toast.Toasting(message, title, type, persist);
-
-                // Console.WriteLine("");
             };
 
             // Démarrer l'écoute
