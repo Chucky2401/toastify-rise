a5f86b18b76016fd465d40447f291b0272fc075a
The Black Wizard <chuckydu24@gmail.com>
2025-04-04 08:06:22 +0200

    chore(Listener): remove old code

--- src/Listener.cs
+++ src/Listener.cs
@@ -9,66 +9,6 @@ using System.Threading.Tasks;
 using System.Web;
 
 namespace toastifyRise {
-    //public class Listener {
-
-    //    private string schema;
-    //    private string listeningAddress;
-    //    private Int32 listeningPort;
-    //    //private Dictionary<string, string>? postParams = new Dictionary<string, string>();
-
-    //    public Listener() {
-    //        schema = "http";
-    //        listeningAddress = "*";
-    //        listeningPort = 13000;
-    //    }
-
-    //    public Dictionary<string, string> Listen() {
-    //        HttpListener server = new HttpListener();
-    //        //postParams.Clear();
-    //        Dictionary<string, string>? postParams = new Dictionary<string, string>();
-
-    //        try {
-    //            server.Prefixes.Add($"{schema}://{listeningAddress}:{listeningPort}/");
-    //            server.Start();
-
-    //            HttpListenerContext context = server.GetContext();
-    //            HttpListenerRequest request = context.Request;
-    //            HttpListenerResponse response = context.Response;
-
-    //            Stream body = request.InputStream;
-    //            Encoding encoding = request.ContentEncoding;
-    //            StreamReader reader = new StreamReader(body, encoding);
-    //            string rawData = reader.ReadToEnd();
-
-    //            string[] rawParams = rawData.Split('&');
-
-    //            foreach (string param in rawParams) {
-    //                string[] kvPair = param.Split('=');
-    //                string key = kvPair[0];
-    //                string value = HttpUtility.UrlDecode(kvPair[1]);
-    //                postParams.Add(key, value);
-    //            }
-
-    //            body.Close();
-    //            reader.Close();
-
-    //            string responseString = "OK";
-    //            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-
-    //            response.ContentLength64 = buffer.Length;
-    //            Stream output = response.OutputStream;
-    //            output.Write(buffer, 0, buffer.Length);
-
-    //            output.Close();
-    //            response.Close();
-    //        } finally {
-    //            server.Stop();
-    //        }
-
-    //        return postParams;
-    //    }
-    //}
-
     public class Listener {
 
         private string schema;
@@ -140,12 +80,6 @@ namespace toastifyRise {
             using (Stream output = response.OutputStream) {
                 output.Write(buffer, 0, buffer.Length);
             }
-
-            // Process the postParams as needed
-            //Console.WriteLine("Received parameters:");
-            //foreach (var param in postParams) {
-            //    Console.WriteLine($"{param.Key}: {param.Value}");
-            //}
 
             RequestReceived?.Invoke(postParams);
         }
